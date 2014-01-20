61bedb797e43e390297fddf46a2a967be54c83c4
Your Name Heremkdir <jsuika2@fullsail.edu>
2014-01-19 19:02:21 -0500

    chapters 1 and 2 from Java Script Book

---
+++ SJ/Sample3.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Suika on 1/17/14.
+ */
+
+
+
+var areaCode =407;
+var firstPart =203;
+var secPart=4529;
+
+var phoneNo= "("+String(areaCode)+")"+ String(firstPart)+"-" +String(secPart);
+console.log(phoneNo);
---
+++ SJ/Sample4.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Suika on 1/18/14.
+ */
+
+
